b03e2267cfeb0addab4550c070164d1cd7202b9d
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-05-20 18:32:41 -0500

    get allMarkdownRemark to work

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,14 +1,49 @@
 import React from "react"
+import { graphql } from 'gatsby'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const SecondPage = ({ data }) => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>My Site's Files</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>relativePath</th>
+          <th>prettySize</th>
+          <th>extension</th>
+          <th>birthTime</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.allFile.edges.map(({ node }, index) => (
+          <tr key={index}>
+            <td>{node.relativePath}</td>
+            <td>{node.prettySize}</td>
+            <td>{node.extension}</td>
+            <td>{node.birthTime}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    allFile {
+      edges {
+        node {
+          relativePath
+          prettySize
+          extension
+          birthTime(fromNow: true)
+        }
+      }
+    }
+  }
+`
 
 export default SecondPage
