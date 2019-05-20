53df05a962f93a0733ccb36179d7bf61dd896a66
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-05-20 18:41:23 -0500

    got remark to work

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,32 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi there</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>{data.allMarkdownRemark.totalCount} Posts</p>
   </Layout>
 )
+
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
