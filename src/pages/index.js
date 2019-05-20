e82427028dc43e88257fccde4e9b3f681984e267
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-05-20 17:46:59 -0500

    tweak index.js file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -10,7 +9,6 @@ const IndexPage = () => (
     <h1>Hi there</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Image />
   </Layout>
 )
 
