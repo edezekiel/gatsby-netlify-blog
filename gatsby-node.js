2a2b26b32e802650d0d2f76cc7e17263883a7e03
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-05-29 12:20:44 -0500

    create tags page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const path = require(`path`)
+const _ = require("lodash")
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -15,26 +16,61 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
+
+  const blogPostTemplate = path.resolve('src/templates/blog-post.js')
+  const tagTemplate = path.resolve("src/templates/tags.js")
+
   return graphql(`
     {
-      allMarkdownRemark {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 2000
+      ) {
         edges {
           node {
             fields {
               slug
             }
+            frontmatter {
+              tags
+            }
           }
         }
       }
-    }`
-  ).then(result => {
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    }
+  `).then(result => {
+    const posts = result.data.allMarkdownRemark.edges
+
+    // Create post detail pages using slugs for the path
+    posts.forEach(({ node }) => {
       createPage({
         path: node.fields.slug,
-        component: path.resolve(`./src/templates/blog-post.js`),
+        component: blogPostTemplate,
         context: {
           slug: node.fields.slug,
         }
+      })
+    })
+
+    // Tag pages:
+    let tags = []
+    // Iterate through each post, putting all found tags into `tags`
+    _.each(posts, edge => {
+      if (_.get(edge, "node.frontmatter.tags")) {
+        tags = tags.concat(edge.node.frontmatter.tags)
+      }
+    })
+    // Eliminate duplicate tags
+    tags = _.uniq(tags)
+
+    // Make tag pages
+    tags.forEach(tag => {
+      createPage({
+        path: `/tags/${_.kebabCase(tag)}/`,
+        component: tagTemplate,
+        context: {
+          tag,
+        },
       })
     })
   })
