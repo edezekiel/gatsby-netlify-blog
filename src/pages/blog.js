be341020973cdb8e3f878130e29f6b03dc26d762
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-05-30 12:31:16 -0500

    tag buttons change displayed articles

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -8,30 +8,52 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 export default class IndexPage extends Component {
+
   state = {
-    currentTags: "fourth"
+    allTags: this.props.data.allMarkdownRemark.group.map(item => item.fieldValue),
+    currentTag: null
+  }
+
+  setTag = (e) => {
+    console.log(e.target.value)
+    this.setState({ currentTag: e.target.value })
   }
 
   render() {
     const data = this.props.data
-
+    console.log(this.state.currentTag)
     return (
       <Layout>
         <SEO title="Blog" />
-        <h2>{this.state.currentTags}</h2>
+        <h2>{this.state.currentTag}</h2>
         <section>
           {data.allMarkdownRemark.group.map(tag => (
-            <div key={tag.fieldValue}>
-              <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
+              <button
+                key={tag.fieldValue}
+                value={tag.fieldValue}
+                onClick={this.setTag}>
                 {tag.fieldValue}
-              </Link>
-            </div>
+              </button>
           ))}
         </section>
 
         <section>
           {data.allMarkdownRemark.edges.map(({ node }) => {
-            if (node.frontmatter.tags.includes(this.state.currentTags)){
+            if (!(this.state.currentTag === null)) {
+              if (node.frontmatter.tags.includes(this.state.currentTag)){
+                return (
+                  <Link to={node.fields.slug}>
+                    <h3 class="post-index" key={node.id}>
+                      {node.frontmatter.title}
+                    </h3>
+                    <p>{node.frontmatter.date}</p>
+                    <p>{node.excerpt}</p>
+                    <p>{node.frontmatter.tags}</p>
+                    <hr />
+                  </Link>
+                )
+              }
+            } else {
               return (
                 <Link to={node.fields.slug}>
                   <h3 class="post-index" key={node.id}>
