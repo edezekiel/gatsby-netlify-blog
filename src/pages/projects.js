3725f89aaab75e823fb309847686dc604ece38a2
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-06-21 14:19:01 -0500

    change IndexPage to a functional component

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,33 +5,75 @@ import SEO from "../components/seo"
 import Project from "../components/project"
 
 const projects = [
-  { title: "first", img: "https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image-300x225.png", description: "todo", link: "/" },
-  { title: "second", img: "https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image-300x225.png", description: "todo", link: "/" },
-  { title: "third", img: "https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image-300x225.png", description: "todo", link: "/" },
-  { title: "fourth", img: "https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image-300x225.png", description: "todo", link: "/" },
-  { title: "fifth", img: "https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image-300x225.png", description: "todo", link: "/" },
-  { title: "sixth", img: "https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image-300x225.png", description: "todo", link: "/" },
-  { title: "seventh", img: "https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image-300x225.png", description: "todo", link: "/" },
+  {
+    title: "first",
+    img:
+      "https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image-300x225.png",
+    description: "todo",
+    link: "/",
+  },
+  {
+    title: "second",
+    img:
+      "https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image-300x225.png",
+    description: "todo",
+    link: "/",
+  },
+  {
+    title: "third",
+    img:
+      "https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image-300x225.png",
+    description: "todo",
+    link: "/",
+  },
+  {
+    title: "fourth",
+    img:
+      "https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image-300x225.png",
+    description: "todo",
+    link: "/",
+  },
+  {
+    title: "fifth",
+    img:
+      "https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image-300x225.png",
+    description: "todo",
+    link: "/",
+  },
+  {
+    title: "sixth",
+    img:
+      "https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image-300x225.png",
+    description: "todo",
+    link: "/",
+  },
+  {
+    title: "seventh",
+    img:
+      "https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image-300x225.png",
+    description: "todo",
+    link: "/",
+  },
 ]
 
-export default class IndexPage extends Component {
-  render() {
-    return (
-      <Layout>
-        <SEO title={"Projects"} />
-        <section className="projectHeader">
-          <h1>My Recent Work</h1>
-          <h2>
-            Here are a few recent design projects. Want to see more?{" "}
-            <a href="mailto:ed.a.ezekiel@gmail.com">Email me</a>.
-          </h2>
-        </section>
-        <section className="projectsContainer">
-          {projects.map(project => {
-            return (<Project project={project} />)
-          })}
-        </section>
-      </Layout>
-    )
-  }
+function IndexPage() {
+  return (
+    <Layout>
+      <SEO title={"Projects"} />
+      <section className="projectHeader">
+        <h1>My Recent Work</h1>
+        <h2>
+          Here are a few recent design projects. Want to see more?{" "}
+          <a href="mailto:ed.a.ezekiel@gmail.com">Email me</a>.
+        </h2>
+      </section>
+      <section className="projectsContainer">
+        {projects.map(project => {
+          return <Project project={project} />
+        })}
+      </section>
+    </Layout>
+  )
 }
+
+export default IndexPage
