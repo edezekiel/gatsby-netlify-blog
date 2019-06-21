ee4e08a8212ee1ed6334409133498e202c6856d7
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-06-21 14:41:53 -0500

    change project cards so that the whole card is a link

--- src/components/project.js
+++ src/components/project.js
@@ -2,16 +2,15 @@ import React from "react"
 
 function Project(props) {
   return (
-    <div className="projectCard">
-      <img className="projectImage" src={props.project.img} />
-      <div className="overlay">
-        <h3>{props.project.title}</h3>
-        <p>{props.project.description}</p>
-        <a href={props.project.link} className="projectLink">
-          Visit Project
-        </a>
+    <a href={props.project.link}>
+      <div className="projectCard">
+        <img className="projectImage" src={props.project.img} />
+        <div className="overlay">
+          <h3>{props.project.title}</h3>
+          <p>{props.project.description}</p>
+        </div>
       </div>
-    </div>
+    </a>
   )
 }
 
