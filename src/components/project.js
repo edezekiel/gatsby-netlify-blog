ff613cce5d7c8b86d7fa8c3cf4825c302ad5192c
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-06-21 16:13:04 -0500

    image files are queried and show up on the projects page

--- src/components/project.js
+++ src/components/project.js
@@ -1,18 +1,31 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+// import Image from "./image"
+// <img className="projectImage" src={props.project.img} />
 
 function Project(props) {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "first.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <a href={props.project.link}>
-
-    <div className="projectCard">
-      <img className="projectImage" src={props.project.img} />
-      <div className="overlay">
+      <div>
+        <Img fluid={data.placeholderImage.childImageSharp.fluid} />
         <h3>{props.project.title}</h3>
         <p>{props.project.description}</p>
       </div>
-    </div>
     </a>
-
   )
 }
 
