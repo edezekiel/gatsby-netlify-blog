c0567d817a2ba0f10560bc07ae9bd2373bf87bd0
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-06-22 22:35:11 -0500

    change the font family, line height, and font size

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -11,7 +11,10 @@ export default ({ data }) => {
   return (
     <Layout>
       <h1 className="postShowPageTitle">{post.frontmatter.title}</h1>
-      <Img className="postShowPageImage" fluid={post.frontmatter.hero.childImageSharp.fluid} />
+      <Img
+        className="postShowPageImage"
+        fluid={post.frontmatter.hero.childImageSharp.fluid}
+      />
       <article>
         <button>
           <a href={editOnGithub(post)} target="blank">
