211ad58b160770801c58a37c366a1bdefe0bef79
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-07-15 10:05:51 -0500

    add twitter icon to the contact page

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,13 @@ function Footer(props) {
   return (
     <footer>
       <section>
-        <h2>Made with{" "}<span className="emoji" role="img" aria-label="emoji">❤️</span>{" "}by Ed Ezekiel</h2>
+        <h2>
+          Made with{" "}
+          <span className="emoji" role="img" aria-label="emoji">
+            ❤️
+          </span>{" "}
+          by Ed Ezekiel
+        </h2>
       </section>
       <section>
         <h2>
@@ -14,13 +20,24 @@ function Footer(props) {
           {` `}
           <a href="https://www.gatsbyjs.org">Netlify</a>
         </h2>
-        <p>
-          Thank you{" "}
-          <a href="https://fontawesome.com/license" target="blank">
-            FontAwesome
+        <div>
+          Icons made by{" "}
+          <a href="https://www.flaticon.com/authors/google" title="Google">
+            Google
           </a>{" "}
-          for the amazing symbols.
-        </p>
+          from{" "}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>{" "}
+          is licensed by{" "}
+          <a
+            href="http://creativecommons.org/licenses/by/3.0/"
+            title="Creative Commons BY 3.0"
+            target="_blank"
+          >
+            CC 3.0 BY
+          </a>
+        </div>
       </section>
     </footer>
   )
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,16 +2,33 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 function ContactPage(props) {
+  const data = useStaticQuery(graphql`
+    query {
+      twitterIcon: file(relativePath: { eq: "twitterIcon.png" }) {
+        childImageSharp {
+          fixed(width: 50, height: 50) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <Layout>
       <SEO title="Contact" />
       <h1>Contact</h1>
-        <a href="#">Twitter</a>
-        <a href="#">Github</a>
-        <a href="#">LinkedIn</a>
-        <a href="#">Email</a>
+
+      <Img fadeIn={true} fixed={data.twitterIcon.childImageSharp.fixed} />
+
+      <a href="#">Twitter</a>
+      <a href="#">Github</a>
+      <a href="#">LinkedIn</a>
+      <a href="#">Email</a>
     </Layout>
   )
 }
