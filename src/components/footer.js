abd448aa85fd8b75f4dbae31a9bf1dceb035bf86
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-07-31 13:11:12 -0500

    rework the footer and contact page styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,44 +1,65 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 function Footer(props) {
+  const data = useStaticQuery(graphql`
+    query {
+      github: file(relativePath: { eq: "github.png" }) {
+        childImageSharp {
+          fixed {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      linkedin: file(relativePath: { eq: "linkedin.png" }) {
+        childImageSharp {
+          fixed {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      twitter: file(relativePath: { eq: "twitter.png" }) {
+        childImageSharp {
+          fixed {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <footer>
-      <section>
-        <h2>
-          Made with{" "}
-          <span className="emoji" role="img" aria-label="emoji">
-            ❤️
-          </span>{" "}
-          by Ed Ezekiel
-        </h2>
-      </section>
-      <section>
-        <h2>
-          {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a> and
-          {` `}
-          <a href="https://www.gatsbyjs.org">Netlify</a>
-        </h2>
-        <div>
-          Icons made by{" "}
-          <a href="https://www.flaticon.com/authors/google" title="Google">
-            Google
-          </a>{" "}
-          from{" "}
-          <a href="https://www.flaticon.com/" title="Flaticon">
-            www.flaticon.com
-          </a>{" "}
-          is licensed by{" "}
-          <a
-            href="http://creativecommons.org/licenses/by/3.0/"
-            title="Creative Commons BY 3.0"
-            target="_blank"
-          >
-            CC 3.0 BY
-          </a>
-        </div>
-      </section>
+      <a className="icon-link" href="https://twitter.com/EdwardAEzekiel">
+        <Img fadeIn={true} fixed={data.twitter.childImageSharp.fixed} />
+      </a>
+      <a className="icon-link" href="https://github.com/edezekiel">
+        <Img fadeIn={true} fixed={data.github.childImageSharp.fixed} />
+      </a>
+      <a className="icon-link" href="https://www.linkedin.com/in/edezekiel/">
+        <Img fadeIn={true} fixed={data.linkedin.childImageSharp.fixed} />
+      </a>
+      <p>©{new Date().getFullYear()} Edward Ezekiel.</p>
+      <p>
+        Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a> and
+        {` `}
+        <a href="https://www.gatsbyjs.org">Netlify</a>.
+      </p>
+      <p>
+        Icons by
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          {" "}
+          Flaticon
+        </a>{" "}
+        and
+        <a href="https://www.iconfinder.com/" title="IconFinder">
+          {" "}
+          IconFinder
+        </a>
+      </p>
     </footer>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,60 +1,39 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 function ContactPage(props) {
-  const data = useStaticQuery(graphql`
-    query {
-      github: file(relativePath: { eq: "github.png" }) {
-        childImageSharp {
-          fixed {
-            ...GatsbyImageSharpFixed
-          }
-        }
-      }
-      linkedin: file(relativePath: { eq: "linkedin.png" }) {
-        childImageSharp {
-          fixed {
-            ...GatsbyImageSharpFixed
-          }
-        }
-      }
-      twitter: file(relativePath: { eq: "twitter.png" }) {
-        childImageSharp {
-          fixed {
-            ...GatsbyImageSharpFixed
-          }
-        }
-      }
-    }
-  `)
-
   return (
     <Layout>
       <SEO title="Contact" />
       <h1>Contact</h1>
-      <p>Let's stay in touch!</p>
-
       <section className="contact-links">
-        <a href="https://twitter.com/EdwardAEzekiel">
-          <Img fadeIn={true} fixed={data.twitter.childImageSharp.fixed} />
-        </a>
-        <a href="https://github.com/edezekiel">
-          <Img fadeIn={true} fixed={data.github.childImageSharp.fixed} />
-        </a>
-        <a href="https://www.linkedin.com/in/edezekiel/">
-          <Img fadeIn={true} fixed={data.linkedin.childImageSharp.fixed} />
-        </a>
-      </section>
-
-      <section>
-        Or email me at{" "}
-        <a className="email-address" href="mailto: ed.a.ezekiel@gmail.com">
-          ed.a.ezekiel@gmail.com
-        </a>
+        <p>Let's stay in touch!</p>
+        <p>
+          I'm on{" "}
+          <a className="contact-link" href="https://twitter.com/EdwardAEzekiel">
+            Twitter
+          </a>
+          ,{" "}
+          <a className="contact-link" href="https://github.com/edezekiel">
+            Github
+          </a>
+          , and{" "}
+          <a
+            className="contact-link"
+            href="https://www.linkedin.com/in/edezekiel/"
+          >
+            LinkedIn
+          </a>
+          .
+        </p>
+        <p>
+          You can also email me at{" "}
+          <a className="contact-link" href="mailto: ed.a.ezekiel@gmail.com">
+            ed.a.ezekiel@gmail.com
+          </a>
+        </p>
       </section>
     </Layout>
   )
