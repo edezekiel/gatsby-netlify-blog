edd026d56272b4bcfac977c3ce2297b4e1ff6932
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-07-31 15:05:05 -0500

    swap project links to my blog posts

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -60,48 +61,80 @@ function ProjectsPage(props) {
         <h2>Here are a few of my recent web projects.</h2>
       </section>
       <section className="projectsContainer">
-        <a href="https://youtube-academy.netlify.com" target="blank" className="projectCard">
-          <Img fadeIn={true} fluid={data.youtubeacademy.childImageSharp.fluid} />
+        <Link className="projectCard" to="/posts/2019-03-06-YouTube-Academy.md">
+          <Img
+            fadeIn={true}
+            fluid={data.youtubeacademy.childImageSharp.fluid}
+          />
           <div className="projectCaption">
             <h3>YouTube Academy</h3>
-            <p>Take YouTube instructional videos to the next level. Save and share notes.</p>
+            <p>
+              Take YouTube instructional videos to the next level. Save and
+              share notes.
+            </p>
           </div>
-        </a>
-        <a href="https://got-game.netlify.com/" target="blank" className="projectCard">
+        </Link>
+
+        <Link className="projectCard" to="/posts/2019-03-14-GOT-Game.md">
           <Img fadeIn={true} fluid={data.gotgame.childImageSharp.fluid} />
           <div className="projectCaption">
             <h3>GOT Game</h3>
-            <p>Test your Game of Thrones knowledge with this serverless React game.</p>
+            <p>
+              Test your Game of Thrones knowledge with this serverless React
+              game.
+            </p>
           </div>
-        </a>
-        <a href="https://www.edezekiel.com/" target="blank" className="projectCard">
+        </Link>
+
+        <Link
+          className="projectCard"
+          to="/posts/2019-02-06-Introducing-My-Upgraded-Blog.md"
+        >
           <Img fadeIn={true} fluid={data.edezekielblog.childImageSharp.fluid} />
           <div className="projectCaption">
             <h3>Ed Ezekiel Blog (v1)</h3>
-            <p>Full stack web app (React/Rails) to showcase my technical blog.</p>
+            <p>
+              Full stack web app (React/Rails) to showcase my technical blog.
+            </p>
           </div>
-        </a>
-        <a href="https://github.com/edezekiel/blocksaver" target="blank" className="projectCard">
+        </Link>
+
+        <Link className="projectCard" to="/posts/2019-03-22-Smart-City-Hackathon.md">
           <Img fadeIn={true} fluid={data.blocksaver.childImageSharp.fluid} />
           <div className="projectCaption">
             <h3>BlockSaver</h3>
-            <p>Geotag and report pedestrian safety issues using the blockchain.</p>
+            <p>
+              Geotag and report pedestrian safety issues using the blockchain.
+            </p>
           </div>
-        </a>
-        <a href="https://daily-gif.herokuapp.com/" target="blank" className="projectCard">
+        </Link>
+
+        <Link
+          className="projectCard"
+          to="/posts/2018-12-07-Pair-Programming.md"
+        >
           <Img fadeIn={true} fluid={data.dailygif.childImageSharp.fluid} />
           <div className="projectCaption">
             <h3>DailyGif</h3>
-            <p>A fun app to upload gifs and scroll through a responsive feed.</p>
+            <p>
+              A fun app to upload gifs and scroll through a responsive feed.
+            </p>
           </div>
-        </a>
-        <a href="https://rubygems.org/gems/kaizoku/versions/0.1.3" target="blank" className="projectCard">
+        </Link>
+
+        <Link
+          className="projectCard"
+          to="/posts/2018-11-24-Kaizoku-My-First-Ruby-Gem.md"
+        >
           <Img fadeIn={true} fluid={data.kaizoku.childImageSharp.fluid} />
           <div className="projectCaption">
             <h3>Kaizoku</h3>
-            <p>Command Line Interface to easily find the best ruby gem for the task.</p>
+            <p>
+              Command Line Interface to easily find the best ruby gem for the
+              task.
+            </p>
           </div>
-        </a>
+        </Link>
       </section>
     </Layout>
   )
