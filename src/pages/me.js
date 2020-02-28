b0dac3302e1a9a3f3ede82c67d31060cccb79cc9
Ed Ezekiel <ed@ubiquisoft.com>
2020-02-28 16:29:44 -0600

    Tweak styling and content in me page

--- src/pages/me.js
+++ src/pages/me.js
@@ -1,6 +1,4 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -11,28 +9,8 @@ function ContactPage(props) {
     <Layout>
       <SEO title="Contact" />
       <article>
-        <h2>About Me</h2>
-
-        <p>
-          I live in Memphis, TN with my wife, daughter, and two dogs (go Grizz!). I work at <a href="https://www.ubiquisoft.com/">Ubiquisoft</a> as 
-          a full-stack software developer.
-        </p>
-
-        <p>
-          My passion for coding began at a legal tech company. I started working
-          there as an attorney, but steadily got hooked on programming.
-        </p>
-
-        <p>
-          In 2018 I enrolled in Flatiron's immersive software engineering
-          bootcamp. It was an incredible experience where I learned JavaScript,
-          React, Redux, Ruby, and Rails. I also got my first taste of peer
-          programming and agile software development.
-        </p>
-      </article>
-
-      <section className="contact-links">
-        <h1>Around the web</h1>
+        <h2>Around the web</h2>
+        <section className="contact-links">
         <ul>
           <li>
             Twitter: <a className="contact-link" href="https://twitter.com/EdwardAEzekiel">
@@ -55,7 +33,29 @@ function ContactPage(props) {
             </a>
           </li>
         </ul>
-      </section>
+        </section>
+      </article>
+
+      <article>
+        <h2>About Me</h2>
+
+        <p>
+          I live in Memphis, TN with my wife, daughter, and two dogs (go Grizz!). I work at <a href="https://www.ubiquisoft.com/">Ubiquisoft</a> as 
+          a full-stack software developer.
+        </p>
+
+        <p>
+          My passion for coding began at a legal tech company. I started working
+          there as an attorney, but steadily got hooked on programming.
+        </p>
+
+        <p>
+          In 2018 I enrolled in Flatiron's immersive software engineering
+          bootcamp. It was an incredible experience where I learned JavaScript,
+          React, Redux, Ruby, and Rails. I also got my first taste of peer
+          programming and agile software development.
+        </p>
+      </article>
     </Layout>
   )
 }
